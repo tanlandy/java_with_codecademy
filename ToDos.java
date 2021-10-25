e5c242ecdf307998b21ec32871cb8eeac4e9efe5
Kunlin Tan <31619845+tanlandy@users.noreply.github.com>
2021-10-25 09:53:00 -0700

    Create ToDos.java
    Java's ArrayList hello world

---
+++ ToDos.java
@@ -0,0 +1,15 @@
+// import the ArrayList package here:
+import java.util.ArrayList;
+
+//ArrayList HelloWorld
+
+class ToDos {
+  
+  public static void main(String[] args) {
+    
+    // Create toDoList below:
+    ArrayList<String> toDoList = new ArrayList<String>();
+    
+  }
+  
+}
