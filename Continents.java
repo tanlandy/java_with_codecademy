a6f9e66d2c709d2108fa7e4ac3c154eb937aea73
Kunlin Tan <31619845+tanlandy@users.noreply.github.com>
2021-10-24 19:05:14 -0700

    Create Continents.java
    Switch operation

---
+++ Continents.java
@@ -0,0 +1,20 @@
+public class Continents {
+  // Learn to use switch
+	public static void main(String[] args) {
+    int continent = 2;
+    switch(continent){
+      case(1):
+      System.out.println("North America: Mexico City, Mexico.");
+      break;
+      case(2):
+      System.out.println("Australia: Sydney, Australia");
+      break;
+      default:
+      System.out.println("Undefined continent!");
+      break;
+    }
+
+		
+
+	}
+}
